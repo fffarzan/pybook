babcb47b764198843e699b70b74be4f68467315d
Farzan <farzan@OFD001M6014.local>
2025-12-04 00:10:18 +0400

    first commit

---
+++ book_outlet/models.py
@@ -0,0 +1,17 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.urls import reverse
+
+# model fields reference
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[self.id])
+    
+
+    def __str__(self):
+        return f'{self.title} ({self.ratinge})'
---
+++ book_outlet/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import Book
+
+def index(request):
+    books = Book.objects.all()
+
+    return render(request, 'book_outlet/index.html', {
+        'books': books
+    })
+
+def book_detail(request, id):
+    book = get_object_or_404(Book, pk=id)
+
+    return render(request, 'book_outlet/book_detail.html', {
+        'title': book.title,
+        'author': book.author,
+        'rating': book.rating,
+        'is_bestseller': book.is_bestselling,
+    })
