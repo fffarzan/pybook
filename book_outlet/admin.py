eed6fb1e56548b5c077b005654d4f4f630c45c85
Farzan <farzan@OFD001M6014.local>
2025-12-05 15:27:23 +0400

    finish relations

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book, Author, Address, Country
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_filter = ('first_name', 'last_name')
+    list_display = ('first_name', 'last_name')
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('author', 'rating')
+    list_display = ('title', 'author', 'rating')
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address)
+admin.site.register(Country)
